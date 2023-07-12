dff6111de26a7dc3cfc77a502ef8e6c7dd55a732
Thanhdc <Thanhdc@softworld.jp>
2023-07-12 23:45:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function App() {
-  const [response, setResponse] = useState('');
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://laraveltest220795-d8fc6767e0c3.herokuapp.com/healthcheck');
-      const data = await response.json();
-      setResponse(JSON.stringify(data));
-    } catch (error) {
-      console.error(error);
-      setResponse('Error occurred while fetching data.');
-    }
-  };
+const App = () => {
+  const imageUrl = 'https://d2zn0b5zde5714.cloudfront.net/logo.png';
+  const domain = window.location.origin;
 
   return (
     <div>
-      <h1>API Response:</h1>
-      <pre>{response}</pre>
+      <h1>Hiển thị ảnh từ URL</h1>
+      <img src={imageUrl} alt="Logo" />
+      <p>{domain}</p>
     </div>
   );
-}
+};
 
 export default App;
