f0975c90f5073e96706ba5511525ea4dce009521
Thanhdc <thanhdc@bap.jp>
2023-07-31 16:53:22 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [showPopup, setShowPopup] = useState(false);
   const [orientation, setOrientation] = useState(0);
+  const handleShowPopup = () => {
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
 
   const handleOrientationChange = (e) => {
     setOrientation(e.target.screen.orientation.angle);
@@ -20,6 +28,7 @@ const App = () => {
   };
 
   useEffect(() => {
+    
     if ("onorientationchange" in window) {
       window.addEventListener("orientationchange", handleOrientationChange);
     }
@@ -42,6 +51,16 @@ const App = () => {
     <div>
       <h1>Orientation Change Detection</h1>
       <p>Current Orientation: {orientation}</p>
+      <button onClick={handleShowPopup}>Show Popup</button>
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <h2>Popup Content</h2>
+            <p>This is the popup that appears when orientation changes.</p>
+            <button onClick={handleClosePopup}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
